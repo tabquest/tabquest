98ea1c5766dfb8c467ff32079c951897dbd6dab7
halith-smh <mohamedhalith.smh@gmail.com>
2024-12-03 23:05:11 +0530

    Setup for Initial devlopment

---
+++ src/background.js
@@ -0,0 +1,9 @@
+(function init() {
+    // chrome.tabs.onCreated.addListener(function (tab) {
+    //     chrome.tabs.update(tab.id, {
+    //         url: chrome.runtime.getURL("index.html"),
+    //     });
+    // });
+    console.log("hello");
+    
+})();
