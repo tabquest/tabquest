0d6eb29adcef6ea05322c87c16259c69fbb7cbf6
halith-smh <mohamedhalith.smh@gmail.com>
2026-03-03 22:36:33 +0530

    chore: Added SWC for Vite Config

--- code/vite.config.mjs
+++ code/vite.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+    plugins: [react()],
     build: {
         exclude: ["manifest.json", "images/*"], // file paths you want to exclude
     },
